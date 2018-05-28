51d16cb44884c11cbdc5d872305f981b710359ff
Jean-Francois Larouche <rouche@gmail.com>
2018-05-28 19:39:29 -0400

    Section 270 uri builder

--- src/main/java/com/resolutech/springrestclient/app/services/ApiServiceImpl.java
+++ src/main/java/com/resolutech/springrestclient/app/services/ApiServiceImpl.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import com.resolutech.springrestclient.api.domain.User;
 import com.resolutech.springrestclient.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class ApiServiceImpl implements  ApiService {
@@ -13,12 +15,18 @@ public class ApiServiceImpl implements  ApiService {
     // @Important rest template example
     private RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    private final String api_url;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     public List<User> getUsers(Integer limit) {
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(api_url).queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(builder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
